2803fa36871867d51519012be9b416464abf3f76
TASoft Applications <info@tasoft.ch>
2017-04-18 14:39:39 +0200

    Create TATokenNames.m

---
+++ token/TATokenNames.m
@@ -0,0 +1,11 @@
+//
+//  TATokenNames.m
+//  PHPKit_2017
+//
+//  Created by Thomas Abplanalp on 13.04.17.
+//  Copyright © 2017 TASoft Applications. All rights reserved.
+//
+
+#import "TATokenNames.h"
+
+// Functions to convert token levels and token codes to strings or other objects and back
